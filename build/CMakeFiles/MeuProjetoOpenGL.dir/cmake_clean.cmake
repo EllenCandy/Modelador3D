79b4f642ab8da0d5cf2d9c3df2c6bc65c77be705
ellen Linux <ellencandy0@gmail.com>
2026-02-05 15:16:25 -0300

    cmake

---
+++ build/CMakeFiles/MeuProjetoOpenGL.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MeuProjetoOpenGL.dir/glad.c.o"
+  "CMakeFiles/MeuProjetoOpenGL.dir/glad.c.o.d"
+  "CMakeFiles/MeuProjetoOpenGL.dir/main.cpp.o"
+  "CMakeFiles/MeuProjetoOpenGL.dir/main.cpp.o.d"
+  "MeuProjetoOpenGL"
+  "MeuProjetoOpenGL.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/MeuProjetoOpenGL.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
